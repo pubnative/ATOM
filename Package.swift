ebbb42b80cede00dc78bd6e3350a2bd2000a686d
Fares Benhamouda <fares.bnhamouda@gmail.com>
2026-01-12 13:49:46 +0100

    use ATOM.xcframework from root.

--- Package.swift
+++ Package.swift
@@ -15,8 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ATOM",
-            url: "https://github.com/pubnative/ATOM/releases/download/3.8.0/ATOM.xcframework.zip",
-            checksum: "0df4260e4c8f7481fb03901033d49abd4cd42bd330037547ff7aa8daa53731e7"
+            path: "ATOM.xcframework"
         )
     ]
 )
